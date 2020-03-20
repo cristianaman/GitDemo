13f2b6e8aeb499b18b34eb7b182d8170cb4bfea9
Amanuel <nani7@tutanota.com>
2020-03-20 02:13:16 -0700

    the first commit

---
+++ src/Methods.java
@@ -0,0 +1,11 @@
+
+public class Methods {
+
+	public int ValidateHeader() 
+	{
+		System.out.println("Header links validated");
+		return 2;
+		
+	}
+	
+}
---
+++ src/Parent.java
@@ -0,0 +1,21 @@
+
+public class Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		//this step prints in the output 
+		System.out.println("I Navigated to home page");
+		
+Methods m=new Methods();
+System.out.println(m.ValidateHeader());
+//Classobjects.methodname
+		
+	
+		
+		
+		
+		
+	}
+
+}
